3244dc2e95fab484c0108c000dfe8ec5b42d0505
nhoward09 <how19012@byui.edu>
2024-02-19 16:12:02 -0700

    deleted copy of final

--- final/scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-// Get reference to the button and the section
-const startButton = document.getElementById('startButton');
-const startMenu = document.getElementById('menu');
-
-// Add event listener to the button
-startButton.addEventListener('click', function() {
-    // Hide the section by changing its display property
-    startMenu.style.display = 'none';
-});
